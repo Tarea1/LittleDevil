b56d7d9e1b06557c8020e2dc11fe9094ebd7b9b2
Jorge <andrespato1@gmail.com>
2014-10-27 21:43:38 +0100

    PROBANDO QUE EL GIT SE INSTALO CORRECTAMENTE

--- src/interfaz/IntTP1.java
+++ src/interfaz/IntTP1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+// PRUEBA DEL NUEVO NETBEANS
 package interfaz;
 
 import interfaz.IntTP1.LD.Reproductor;
